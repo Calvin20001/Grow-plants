3be03afd233bd3e62e220bead83303fe31b4bd10
Calvin20001 <ccheng20001@gmail.com>
2025-08-17 17:15:01 -0500

    fixes

--- web/worker.js
+++ web/worker.js
@@ -1,20 +1,27 @@
 // Cloudflare Workers script for Grow Plants game
 export default {
   async fetch(request, env, ctx) {
-    const url = new URL(request.url);
-    const path = url.pathname;
-    
-    // Default to index.html
-    if (path === '/' || path === '') {
-      return env.ASSETS.fetch(new Request(`${url.origin}/index.html`, request));
-    }
-    
-    // Try to serve the requested file
     try {
-      return await env.ASSETS.fetch(request);
-    } catch (e) {
+      const url = new URL(request.url);
+      const path = url.pathname;
+      
+      // Default to index.html for root path
+      if (path === '/' || path === '') {
+        return env.ASSETS.fetch(new Request(`${url.origin}/index.html`, request));
+      }
+      
+      // Try to serve the requested file
+      const response = await env.ASSETS.fetch(request);
+      
       // If file not found, return index.html for SPA routing
-      return env.ASSETS.fetch(new Request(`${url.origin}/index.html`, request));
+      if (response.status === 404) {
+        return env.ASSETS.fetch(new Request(`${url.origin}/index.html`, request));
+      }
+      
+      return response;
+    } catch (error) {
+      // Fallback to index.html on any error
+      return env.ASSETS.fetch(new Request(`${request.url.split('/')[0]}//${request.url.split('/')[2]}/index.html`, request));
     }
   }
 };
